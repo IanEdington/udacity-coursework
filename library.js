a8fb88756d082e1a4f0e6e4e1c8d9b4df7735293
Ian Edington <ian@edington.ca>
2015-05-10 21:22:52 -0400

    run game - make more functions

--- library.js
+++ library.js
@@ -1,9 +1,13 @@
 var carlike = function(loc){
   var obj = {loc: loc};
   obj.move = move;
+  obj.on = on;
+  obj.off = off;
   return obj;
 };
 
 var move = function(){
   this.loc += 1;
 };
+var on = function(){ /* some function */ }
+var off = function(){ /* some function */ }
