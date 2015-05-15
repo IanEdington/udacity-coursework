4f319ea886bd550abc8cd4e9e824104652e0cf29
Ian Edington <ian@edington.ca>
2015-05-15 12:01:03 -0400

    got the clicker working - next work on formatting

--- cat-clicker-duo/js/cat-clicker-lib.js
+++ cat-clicker-duo/js/cat-clicker-lib.js
@@ -1,4 +1,4 @@
-var HTMLcat = '<div id="Cat-%catname%" class="col-md-6"> <div id="%catname%-header" class="center-content clearfix"> </div> <div id="%catname%-cat-pic"> <img src="%url%" class="img-responsive"></div><div id="%catname%-Counter" class="%class%"> 0 </div> </div>';
+var HTMLcat = '<div id="Cat-%catname%" class="col-md-6"> <div id="%catname%-header" class="center-content clearfix"> </div> <div id="%catname%-cat-pic"> <img src="%url%" class="img-responsive"></div><div id="%catname%-Counter" class="counter"> 0 </div> </div>';
 
 var Cat = function(catname, url){
 	this.catname = catname;
@@ -9,10 +9,12 @@ var Cat = function(catname, url){
   this.HTML = HTMLcat.replace(/%catname%/g, catname).replace(/%url%/g, url);
 };
 Cat.prototype.clicker = function(){
-	$('#'+this.countName).click(function(e) {
-    this.counter+=1;
-		$('#'+this.countName).html(this.counter.toString());
-    console.log('clicker was clicked' + this.counter);
+  var counter = this.counter;
+  var countName = this.countName;
+	$('#'+this.catPic).click(function(e) {
+    console.log('clicker was clicked' + counter);
+    counter+=1;
+		$('#'+countName).html(counter);
 	});
   console.log('clicker was called');
 };
--- cat-clicker-duo/js/cat-clicker.js
+++ cat-clicker-duo/js/cat-clicker.js
@@ -1,3 +1,7 @@
 var ruby = new Cat('ruby', 'cats/ruby.JPG');
 $('#cat-row1').html(ruby.HTML);
 ruby.clicker();
+
+var jake = new Cat('jake', 'cats/jake.JPG');
+$('#cat-row1').append(jake.HTML);
+jake.clicker();
