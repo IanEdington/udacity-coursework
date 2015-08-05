9a33a14fc25bf583314c0a52ec734d0df0cd2c68
Ian Edington <IanEdington@gmail.com>
2015-08-05 13:45:27 -0400

    vaildity check and spliting file

--- Lesson_3_Data_Quality/12-Correcting_Validity/validity.py
+++ Lesson_3_Data_Quality/12-Correcting_Validity/validity.py
@@ -7,40 +7,79 @@ The following things should be done:
 - the rest of the fields and values should stay the same
 - if the value of the field is a valid year in range, as described above,
   write that line to the output_good file
-- if the value of the field is not a valid year, 
+- if the value of the field is not a valid year,
   write that line to the output_bad file
 - discard rows (neither write to good nor bad) if the URI is not from dbpedia.org
 - you should use the provided way of reading and writing data (DictReader and DictWriter)
   They will take care of dealing with the header.
 
-You can write helper functions for checking the data and writing the files, but we will call only the 
+You can write helper functions for checking the data and writing the files, but we will call only the
 'process_file' with 3 arguments (inputfile, output_good, output_bad).
 """
 import csv
-import pprint
+from pprint import pprint
 
 INPUT_FILE = 'autos.csv'
 OUTPUT_GOOD = 'autos-valid.csv'
 OUTPUT_BAD = 'FIXME-autos.csv'
 
+def row_valid(row):
+    '''row is a dict from
+
+    - check if the field "productionStartYear" contains a year
+    - check if the year is in range 1886-2014
+    - convert the value of the field to be just a year (not full datetime)
+    - the rest of the fields and values should stay the same
+    - if the value of the field is a valid year in range, as described above, write that line to the output_good file
+    - if the value of the field is not a valid year, write that line to the output_bad file
+    - discard rows (neither write to good nor bad) if the URI is not from dbpedia.org
+
+    return: valid, modified_row
+    valid:
+        0 - good
+        1 - bad
+        2 - discard
+    '''
+    try:
+        row['productionStartYear'] = int(row['productionStartYear'][0:4])
+    except:
+        pass
+    if 'dbpedia.org' not in row['URI']:
+        return 2, row
+    if row['productionStartYear'] not in range(1886,2014):
+        return 1, row
+    return 0, row
+
 def process_file(input_file, output_good, output_bad):
+    input_file = INPUT_FILE
+    output_good_data = []
+    output_bad_data = []
+    # output_delete = []
 
     with open(input_file, "r") as f:
+        # f = open(input_file, "r")
         reader = csv.DictReader(f)
         header = reader.fieldnames
+        for row in reader:
+            valid, rowi = row_valid(row)
+            if valid == 0:
+                output_good_data.append(rowi)
+            elif valid == 1:
+                output_bad_data.append(rowi)
+            # else:
+            #     output_delete.append(rowi)
 
-        #COMPLETE THIS FUNCTION
-
-
-
-    # This is just an example on how you can use csv.DictWriter
-    # Remember that you have to output 2 files
-    with open(output_good, "w") as g:
-        writer = csv.DictWriter(g, delimiter=",", fieldnames= header)
+    with open(output_good, "w") as f_out:
+        writer = csv.DictWriter(f_out, delimiter=",", fieldnames= header)
         writer.writeheader()
-        for row in YOURDATA:
+        for row in output_good_data:
             writer.writerow(row)
 
+    with open(output_bad, "w") as f_out:
+        writer = csv.DictWriter(f_out, delimiter=",", fieldnames= header)
+        writer.writeheader()
+        for row in output_bad_data:
+            writer.writerow(row)
 
 def test():
 
